5f2d887a021a5f8aa7df9835be45af74e27ce5ec
andresperezfc <felipe3apc@gmail.com>
2020-05-19 19:11:08 -0500

    149 floatinActionButton y tema de la aplicación

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QRScannerApp',
-      initialRoute: 'home',
-      routes: {'home': (BuildContext context) => HomePage()},
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'QRScannerApp',
+        initialRoute: 'home',
+        routes: {'home': (BuildContext context) => HomePage()},
+        theme: ThemeData(
+          primaryColor: Colors.redAccent,
+        ));
   }
 }
